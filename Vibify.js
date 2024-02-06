bb388f8329f17229673bca7f37c0e35034b695c4
Justin Jongstra <00300061@rijnijssel.nl>
2024-02-06 14:20:37 +0100

    added authenticateApiKey to all routes

--- Vibify.js
+++ Vibify.js
@@ -44,35 +44,35 @@ app.get('/spotify/user/:id', authenticateApiKey, catchErrors(async (req, res) =>
     res.json(user);
 }));
 
-app.get('/spotify/currently-playing/:id', catchErrors(async (req, res) => {
+app.get('/spotify/currently-playing/:id', authenticateApiKey, catchErrors(async (req, res) => {
     const currentlyPlaying = await spotify.getCurrentlyPlaying(req.params.id);
     res.json(currentlyPlaying);
 }));
 
-app.get('/spotify/top-tracks/:id', catchErrors(async (req, res) => {
+app.get('/spotify/top-tracks/:id', authenticateApiKey, catchErrors(async (req, res) => {
     const topTracks = await spotify.getTopTracks(req.params.id);
     res.json(topTracks);
 
 }));
 
-app.get('/spotify/top-artists/:id', catchErrors(async (req, res) => {
+app.get('/spotify/top-artists/:id', authenticateApiKey, catchErrors(async (req, res) => {
     const topArtists = await spotify.getTopArtists(req.params.id);
     res.json(topArtists);
 }));
 
-app.get('/spotify/recently-played/:id', catchErrors(async (req, res) => {
+app.get('/spotify/recently-played/:id', authenticateApiKey, catchErrors(async (req, res) => {
     const recentlyPlayed = await spotify.getLastListenedTracks(req.params.id);
     res.json(recentlyPlayed);
 }));
 
-app.post('/spotify/create-playlist', catchErrors(async (req, res) => {
+app.post('/spotify/create-playlist', authenticateApiKey, catchErrors(async (req, res) => {
     const { id, month, year, playlistName } = req.body;
     const playlist = await spotify.createPlaylist(id, month, year, playlistName);
     res.json(playlist);
 }));
 
 
-app.post('/spotify/recommendations', catchErrors(async (req, res) => {
+app.post('/spotify/recommendations', authenticateApiKey, catchErrors(async (req, res) => {
     const { id, genre, recentlyPlayed, mostPlayed, likedSongs } = req.body;
     const playlist = await spotify.createRecommendationPlaylist(id, genre, recentlyPlayed, mostPlayed, likedSongs);
     res.json(playlist);
