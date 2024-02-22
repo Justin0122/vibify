ba5127fe51c7356d045364e7b8ece0b01ee26c88
Justin Jongstra <00300061@rijnijssel.nl>
2024-02-22 11:28:02 +0100

    fixed createdb script / removed unused env value

--- db/createdb.js
+++ db/createdb.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
-const db = require('./database.js');
+const knex = require('knex');
 
-db.raw('CREATE DATABASE IF NOT EXISTS vibify')
+const db = knex({
+    client: 'mysql',
+    connection: {
+        host: process.env.DB_HOST || '127.0.0.1',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASS || '',
+    },
+});
+
+db.raw('CREATE DATABASE IF NOT EXISTS ??', process.env.DB_NAME)
     .then(() => console.log('Database created'))
     .catch((err) => console.log('Error creating database: ', err))
     .finally(() => db.destroy());
