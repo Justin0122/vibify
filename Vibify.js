d61a8e26a3e37139e8be5e24c2d3da9d7179bbb6
Justin Jongstra <00300061@rijnijssel.nl>
2024-04-23 15:45:45 +0200

    moved setupCors to the top

--- Vibify.js
+++ Vibify.js
@@ -5,9 +5,9 @@ const setupCors = require('./middlewares/setupCors');
 
 
 const app = express();
+app.use(setupCors);
 app.use(express.json());
 app.use('/', routes);
-app.use(setupCors);
 
 app.use((req, res, next) => {
     res.status(404).json({error: '404: Not found'});
